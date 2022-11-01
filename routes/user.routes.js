916e2519bd98d894e26c4facacbc4aa8b3234e86
Jakkapong Karndee <6222782714@g.siit.tu.ac.th>
2022-11-01 20:43:26 +0700

    update home, add unfollow, add follow, change post db

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,9 +14,9 @@ module.exports = function (app) {
 
     app.get("/api/content/user", [authJwt.verifyToken], controller.userBoard);
 
-    app.post("/api/content/post", [authJwt.verifyToken], controller.post);
+    app.post("/api/content/insertpost", [authJwt.verifyToken], controller.insertPost);
 
-    app.post("/api/content/home", controller.home);
+    app.post("/api/content/home", [authJwt.verifyToken], controller.home);
 
     app.get("/api/content/profile", [authJwt.verifyToken], controller.profile);
 
@@ -25,5 +25,7 @@ module.exports = function (app) {
     app.post("/api/content/editpost", [authJwt.verifyToken], controller.editpost);
 
     app.post("/api/content/follow", [authJwt.verifyToken], controller.follow);
+
+    app.post("/api/content/unfollow", [authJwt.verifyToken], controller.unfollow);
 
 };
